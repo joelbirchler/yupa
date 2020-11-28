d5c52c507e77d559bfcf30b81667942c4d62178a
joelbirchler <jbirchler@redhat.com>
2020-11-27 16:32:54 -0800

    sends data to adafruit.io

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4
 	gobot.io/x/gobot v1.14.0
+	k8s.io/apimachinery v0.19.4
 )
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,13 +14,78 @@ import (
 	"github.com/joelbirchler/yupa/pkg/pms5003"
 	"github.com/joelbirchler/yupa/pkg/rgbmatrix"
 	"gobot.io/x/gobot/platforms/raspi"
+	"k8s.io/apimachinery/pkg/util/json"
 )
 
 type adafruitIo struct {
 	username, key, group string
 }
 
+type feed struct {
+	Key   string `json:"key"`
+	Value uint16 `json:"value"`
+}
+
+type createData struct {
+	Feeds []feed `json:"feeds"`
+}
+
+func (a adafruitIo) send(pm25, pm100, e uint16) error {
+	client := http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	url := fmt.Sprintf("https://io.adafruit.com/api/v2/%s/groups/%s/data", a.username, a.group)
+
+	foo := createData{
+		Feeds: []feed{
+			{"Environment25", pm25},
+			{"Environment100", pm100},
+			{"Error", e},
+		},
+	}
+
+	data, err := json.Marshal(foo)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("obj: %v", foo)
+	log.Printf("json: %s", data)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-AIO-Key", a.key)
+
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	log.Println(string(body)) // FIXME: we can ignore the body
+
+	return nil
+}
+
+var aio adafruitIo
+
 func init() {
+	aio = adafruitIo{os.Getenv("AIO_USERNAME"), os.Getenv("AIO_KEY"), os.Getenv("AIO_GROUP")}
+	if aio.username == "" || aio.key == "" || aio.group == "" {
+		log.Fatalln("Environment variables AIO_USERNAME, AIO_KEY, and AIO_GROUP are required. See the README for more information.")
+	}
+
 	// trap sigterm to attempt to close before exiting
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -29,11 +98,6 @@ func init() {
 }
 
 func main() {
-	aio := adafruitIo{os.Getenv("AIO_USERNAME"), os.Getenv("AIO_KEY"), os.Getenv("AIO_GROUP")}
-	if aio.username == "" || aio.key == "" || aio.group == "" {
-		log.Fatalln("Environment variables AIO_USERNAME, AIO_KEY, and AIO_GROUP are required. See the README for more information.")
-	}
-
 	pi := raspi.NewAdaptor()
 
 	if err := rgbmatrix.Open(pi); err != nil {
@@ -44,20 +108,26 @@ func main() {
 		log.Fatalf("unable to setup pms5003: %v", err)
 	}
 
+	var errorCount uint16
+
 	for {
 		f, err := pms5003.ReadFrame()
 		if err != nil {
 			log.Printf("error reading frame: %v", err)
-			// continue
+			errorCount++
 		}
 
 		log.Printf("%+v", f)
+
+		if err := aio.send(f.Environment25, f.Environment100, errorCount); err != nil {
+			log.Printf("sending error: %v", err)
+		}
 
 		rgbmatrix.Set(rgbmatrix.Binary(f.Environment25))
 		if err := rgbmatrix.Render(); err != nil {
 			log.Printf("rendering error: %v", err)
 		}
 
-		time.Sleep(time.Second)
+		time.Sleep(time.Second * 30)
 	}
 }
